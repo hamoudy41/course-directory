317eec3881ab5142db3c13522ad67bc7fc297cfa
hamoudy41 <hamoudy41@yahoo.com>
2020-03-14 21:41:20 +0100

    Updates

---
+++ precache-manifest.a25232c5c4721dbb833021c25e1510d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35bd6edaba51014ae86f1d21624de6d4",
+    "url": "/course-directory/index.html"
+  },
+  {
+    "revision": "41af476fe1c40f0d6657",
+    "url": "/course-directory/static/css/main.3a22b66b.chunk.css"
+  },
+  {
+    "revision": "229af067f11e6bfb3b2b",
+    "url": "/course-directory/static/js/2.befe57ed.chunk.js"
+  },
+  {
+    "revision": "b73db60337a63448fc0f0ed577e37ab5",
+    "url": "/course-directory/static/js/2.befe57ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41af476fe1c40f0d6657",
+    "url": "/course-directory/static/js/main.57bd81b1.chunk.js"
+  },
+  {
+    "revision": "9b0a6b0969f62d815dd1",
+    "url": "/course-directory/static/js/runtime-main.c5d9c68a.js"
+  }
+]);
