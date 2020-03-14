ef37904eb98f7857c5d5f648d05fb5b9a4331054
hamoudy41 <hamoudy41@yahoo.com>
2020-03-14 22:01:53 +0100

    basename

--- src/components/App.js
+++ src/components/App.js
@@ -15,7 +15,7 @@ import NotFound from './NotFound';
 import Featured from './Featured';
 
 const App = () => (
-  <HashRouter>
+  <HashRouter basename="/course-directory">
     <div className="container">
       <Header />
       
